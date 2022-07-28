738b3b848b84b7fff0635e690388a3a53d20def9
Atsushi KAWASAKI <ak@xlix.org>
2022-07-29 02:58:48 +0900

    create a very simple counter.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { createSignal } from 'solid-js';
 import type { Component } from 'solid-js';
 
 const App: Component = () => {
+  const [count, setCount] = createSignal(0);
+  const increment = () => setCount(c => c + 1);
+  const decrement = () => setCount(c => c - 1);
+
   return (
-      <>
-      </>
+    <div class="m-0 w-screen h-screen flex flex-col justify-center items-center gap-4">
+      <button class="p-4 bg-slate-500 rounded-xl font-mono text-4xl" onclick={increment}>+</button>
+      <div class="p-8 font-mono text-9xl text-center">{count()}</div>
+      <button class="p-4 bg-slate-500 rounded-xl font-mono text-4xl" onclick={decrement}>-</button>
+    </div>
   );
 };
 
